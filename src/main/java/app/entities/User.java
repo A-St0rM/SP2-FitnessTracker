8f5660dc73ebfb1bd9d66d9562a1f80687f0356c
AlissaStorm <Alissaalidib@hotmail.com>
2025-10-21 15:55:56 +0200

    changes

--- src/main/java/app/entities/User.java
+++ src/main/java/app/entities/User.java
@@ -1,48 +1,20 @@
 package app.entities;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.mindrot.jbcrypt.BCrypt;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.*;
-
-
-@Getter
-@Setter
 @Entity
-@Builder
-@AllArgsConstructor
+@Data
 @NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true, nullable = false)
     private String email;
-
-    @Column(nullable = false)
-    private String password;
-
-    private double userWeight;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Workout> workouts = new HashSet<>();
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Goal> goals = new HashSet<>();
-
-
-    public User(String email, String password){
-        this.email = email;
-        String hashed = BCrypt.hashpw(password, BCrypt.gensalt());
-        this.password = hashed;
-    }
-
-    public boolean verifyPassword(String pw) {
-        return BCrypt.checkpw(pw, password);
-    }
-
 }
---
+++ src/main/java/app/entities/WorkoutPlan.java
@@ -0,0 +1,31 @@
+package app.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "workout_plans")
+public class WorkoutPlan {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    private User user;
+
+    // Ingen "Weekday" tabel – øvelser peger på plan + weekday
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true)
+    @OrderBy("weekday asc, position asc")
+    private List<WorkoutExercise> exercises = new ArrayList<>();
+
+
+}
